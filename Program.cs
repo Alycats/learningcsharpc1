7388b52caf3005f45cd19b913eb6b03f8bdab5d2
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 13:11:26 +0000

    Base

--- Program.cs
+++ Program.cs
@@ -4,15 +4,19 @@ class Program
     static void Main(string[] args)
     {
 string username;
-
+int responsetwo;
 int response;
+
+
 
         Console.WriteLine("What is your name?");
         username = Console.ReadLine();
         Console.WriteLine("What is your year of birth written xxxx?");
         response = Convert.ToInt32( Console.ReadLine() );
-        
-        Console.WriteLine($"Goodbye, {username}!");
+        Console.WriteLine("Have you had a birthday? Type yes=1 or no=2.");
+        responsetwo = Convert.ToInt32(Console.ReadLine() );
+
+
         Console.ReadKey(true);
     }
 }
