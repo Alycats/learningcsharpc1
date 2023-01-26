8390871082197d6f705a0fface6a6f7a19289ce6
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 15:01:12 +0000

    Age Calculation

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        conditions();
+        BirthdayAge();
         Console.ReadKey(true);
     }
 
@@ -12,9 +12,17 @@ class Program
     {
         //check if x is larger than y
         int x = 20;
-        int y = 2;
+        int y = 20;
 
-        if (x > y)
+        if (x == y)
+        {
+            Console.WriteLine("x and y are equal");
+        }
+        else if(y > x)
+        {
+             Console.WriteLine("y is greater than x");
+        }
+        else
         {
             Console.WriteLine("x is greater than y");
         }
@@ -26,9 +34,10 @@ class Program
     static void BirthdayAge()
     {
         string name;
-        string birthdayTHisYear;
-        int yearOfBirth;
-
+        string birthdayTHisYear = "yes";
+        int yearOfBirth =2000;
+        int year = DateTime.Now.Year;
+        int age =1000;
 
 
         Console.WriteLine("What is your name?");
@@ -36,7 +45,34 @@ class Program
         Console.WriteLine("What is your year of birth written xxxx?");
         yearOfBirth = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Have you had a birthday? Type yes or no.");
-        birthdayTHisYear = Console.ReadLine();
+        birthdayTHisYear = Console.ReadLine().ToLower();
+
+if (birthdayTHisYear =="yes" || birthdayTHisYear=="y")
+{
+    age = year - yearOfBirth;
+    Console.WriteLine($"Age = {age}");
+}
+else 
+{
+    age = year - yearOfBirth-1;
+    Console.WriteLine($"Age = {age}");
+}
+if (age < 18)
+{
+    Console.WriteLine($"You are a junior {name}");
+}
+else if (age >= 18 && age < 30)
+    {
+          Console.WriteLine($"You are a young adult {name}");
+    }
+else if (age >= 30 && age < 60)
+    {
+          Console.WriteLine($"You are an adult {name}");
+    }
+    else
+    {
+        Console.WriteLine($"You are a senior {name}");
+    }
     }
 
     }
