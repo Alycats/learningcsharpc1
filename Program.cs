ac268962d9fe10c81d12fbf7f106214b1fb26164
Alycat <Arc183@student.aru.ac.uk>
2023-01-26 12:24:17 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace learningcsharpc1;
+class Program
+{
+    static void Main(string[] args)
+    {
+string username;
+
+int response;
+
+        Console.WriteLine("What is your name?");
+        username = Console.ReadLine();
+        Console.WriteLine("What is your year of birth written xxxx?");
+        response = Convert.ToInt32( Console.ReadLine() );
+        
+        Console.WriteLine($"Goodbye, {username}!");
+        Console.ReadKey(true);
+    }
+}
